2c7b6e8e9cc04ae570306cb1c5abe9e37d622018
Ezequiel24123z <ezequiel24123@outlook.es>
2025-05-23 18:53:29 +0200

    Renombrado Book.java a Libro.java e implementación de métodos
    Configurado y cambiado Book.java a Libro.java, para una diferenciación clara con respecto al código original. Implementación de atributos, constructor, getters, setters y métodos toString y equals (para comparación de ISBNs)

---
+++ src/main/java/es/ing/tomillo/library/model/Libro.java
@@ -0,0 +1,78 @@
+package es.ing.tomillo.library.model;
+
+public class Libro {
+
+    // Atributos que tendrá la clase Libro
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponibilidad;
+
+    // Implementación del constructor
+    public Libro(String titulo, String autor, String isbn, boolean disponibilidad) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponibilidad = disponibilidad;
+    }
+
+    // Implementación de los getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    // Implementación de los setters
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    // Implementación del metodo toString
+    @Override // Se sobreescribe el metodo toString que tienen todos los tipos de manera predeterminada para la clase "Libro"
+    public String toString() {
+        return "Libro{" + "titulo=" + titulo + ", autor=" + autor + ", isbn=" + isbn + ", disponibilidad=" + disponibilidad + '}';
+    }
+
+
+    // Implementar metodo equals para comparar libros por ISBN
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false; // Verifica si el libro (obj) existe, asi evitamos un posible error (NullPointerException)
+
+        if (!(obj instanceof Libro)) return false; //Verifica si es un libro (obj) sea del tipo "Libro"
+
+        Libro segundoLibro = (Libro) obj; //Convierte el tipo "obj" a tipo "Libro", lo que le permite usar metodos de "Libro"
+
+        if (this.isbn == null)
+            return segundoLibro.getIsbn() == null; //Si los dos ISBN son Null, da True
+
+        return this.isbn.equals(segundoLibro.getIsbn()); //Devuelve un boolean (true o false) en la comparación de los dos libros
+
+    }
+}
+
